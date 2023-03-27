1c2a5e8aca57142586160f70d97a241cde86fb96
sdplex <ehddltkf@naver.com>
2023-03-27 14:03:28 +0900

    main test

---
+++ create_oncue_pipe/src/main/java/com/example/demo/Main.java
@@ -0,0 +1,17 @@
+// Created on iPad.
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 2;
+
+        for (int i =0; b <=5; b++){
+            a = a+b;
+
+        }	
+        System.out.println(a);
+        
+    }
+}
